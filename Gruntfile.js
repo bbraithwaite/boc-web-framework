a7ff601ae90ded391ee7b97bd059793d9f0358c8
bbraithwaite <bradley.braithwaite@gmail.com>
2015-03-11 03:47:53 +0100

    initial spike

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %>' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'lib/*.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      beforeconcat: ['lib/**/*.js', 'test/**/*.js'],
+      afterconcat: ['build/<%= pkg.name %>.js']
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'jshint']);
+
+};
